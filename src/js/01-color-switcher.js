a065d3bece4aa7ca9147517ae78988ab526b9a0f
OlgaSld <125216168+OlgaSld@users.noreply.github.com>
2023-07-07 22:19:31 +0300

    check task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,41 @@
 
+// Напиши скрипт, який після натискання кнопки «Start», раз на секунду змінює колір фону <body> на випадкове значення, використовуючи інлайн стиль. 
+// Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.
+
+// УВАГА
+// Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною (disabled).
+
+// Для генерування випадкового кольору використовуй функцію getRandomHexColor.
+
+
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    bodyColor: document.querySelector('body'),
+}
+let timerId = null;
+
+// console.log(refs.startBtn);
+// console.log(refs.stopBtn);
+
+refs.startBtn.addEventListener('click', changeColor);
+refs.stopBtn.addEventListener('click', stopChangeColor);
+
+function changeColor(e) {
+    timerId = setInterval(() => {
+    e.target.disabled = true;
+    refs.bodyColor.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+}
+
+function stopChangeColor() {
+    clearInterval(timerId);
+    refs.startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+
+
---
+++ src/js/02-timer.js
@@ -0,0 +1,91 @@
+// 1. Встановити бібліотекі
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+// 2.Ініціалізація змінних
+const refs = {
+    btn: document.querySelector('[data-start'),
+    day: document.querySelector('[data-days]'),
+    hour: document.querySelector('[data-hours]'),
+    min: document.querySelector('[data-minutes]'),
+    sec: document.querySelector('[data-seconds]'),
+} 
+
+let timerId = null; 
+
+refs.btn.disabled = true;
+
+// 3.Функція встановлення дати
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= Date.now()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      // window.alert('Please choose a date in the future'); 
+    } else {      
+      refs.btn.disabled = false;
+      Notiflix.Notify.success('Ok, please click Start',
+      {
+         timeout: 1000,
+      },
+    )}
+    console.log(selectedDates[0]);
+  },
+};
+ 
+const flPickr = flatpickr("#datetime-picker", options);
+
+refs.btn.addEventListener('click', onStartClick);
+
+function onStartClick() {
+  // console.log(`click on button`);
+  const selectedDates = flPickr.selectedDates[0];
+
+  timerId = setInterval(() => {
+    const currentDate = new Date();
+    const timeTimer = selectedDates - currentDate;
+    refs.btn.disabled = true;
+
+    const { days, hours, minutes, seconds } = convertMs(timeTimer);
+
+    refs.day.textContent = addLeadingZero(days);
+    refs.hour.textContent = addLeadingZero(hours);
+    refs.min.textContent = addLeadingZero(minutes);
+    refs.sec.textContent = addLeadingZero(seconds);
+
+    if (timerId < 1000) {
+    clearInterval(timerId);
+    return
+    }
+  }, 1000)
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+} 
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
